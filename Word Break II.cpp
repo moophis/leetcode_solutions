3f001e67e1c146a3bc6f8cf80c01f17127eb75e4
moophis <moophis@gmail.com>
2014-05-04 13:03:39 -0700

    Word Break II: should predicate whether solution exists

---
+++ Word Break II.cpp
@@ -0,0 +1,56 @@
+// http://oj.leetcode.com/problems/word-break-ii/
+
+class Solution {
+    void do_break(string &s, int begin, string &buf, 
+                  vector<string> &ans, unordered_set<string> &dict) {
+        int size = static_cast<int>(s.size());
+        
+        if (begin >= size) {
+            int bufsize = buf.size();
+            ans.push_back(buf.substr(0, bufsize - 1));
+        }
+        
+        for (int i = 1; begin + i <= size; i++) {
+            string cur_word = s.substr(begin, i);
+            
+            if (dict.count(cur_word) > 0) {
+                int last_len = static_cast<int>(buf.size());
+                
+                buf += cur_word;
+                buf += " ";
+                do_break(s, begin + i, buf, ans, dict);
+                buf.erase(last_len);
+            }
+        }
+    }
+
+public:
+    vector<string> wordBreak(string s, unordered_set<string> &dict) {
+        vector<string> ans;
+        string buf = "";
+        
+        if (s.size() == 0 || dict.size() == 0) {
+            return ans;
+        }
+        
+        // we should first check out whether the given
+        // string can be broke and match the items in 
+        // the dictionary accordingly.
+        bool dpbuf[s.size()+1];
+        memset(dpbuf, false, s.size()+1);
+        dpbuf[0] = true;
+        for (size_t i = 1; i <= s.size(); i++) {
+            for (size_t j = 0; j < i; j++) {
+                if (dpbuf[j] && dict.count(s.substr(j, i - j)) > 0) {
+                    dpbuf[i] = true;
+                }
+            }
+        }
+        if (!dpbuf[s.size()]) {
+            return ans;
+        } 
+        
+        do_break(s, 0, buf, ans, dict);
+        return ans;
+    }
+};
