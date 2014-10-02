dfb2a7ee50328d226e33813d4ab7b548255e9280
moophis <moophis@gmail.com>
2014-10-01 23:07:10 -0700

    Recover Binary Search Tree: O(1) space

--- Recover Binary Search Tree.cpp
+++ Recover Binary Search Tree.cpp
@@ -1,6 +1,6 @@
 // https://oj.leetcode.com/problems/recover-binary-search-tree/
 
-// Better solution: keep track of two wrong nodes
+// Optimal: use Morris Traversal to achieve O(1) space complexity
 /**
  * Definition for binary tree
  * struct TreeNode {
@@ -10,6 +10,56 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+class Solution {
+    void validate_node(TreeNode* &cur, TreeNode* &first, TreeNode* &second, TreeNode* &last) {
+        if (first == nullptr && cur->val < last->val) {
+            first = last;
+        }
+        if (first != nullptr && cur->val < last->val) {
+            second = cur;
+        }
+        last = cur;
+    }
+    
+    void do_recover(TreeNode *root, TreeNode* &first, TreeNode* &second, TreeNode* &last) {
+        if (root == nullptr) { 
+            return;
+        }
+        
+        TreeNode *cur = root;
+        while (cur != nullptr) {
+            if (cur->left != nullptr) {
+                TreeNode *prob = cur->left;
+                
+                while (prob->right != nullptr && prob->right != cur) {
+                    prob = prob->right;
+                }
+                if (prob->right == nullptr) {
+                    prob->right = cur;
+                    cur = cur->left;
+                } else if (prob->right == cur) { 
+                    prob->right = nullptr;
+                    validate_node(cur, first, second, last);
+                    cur = cur->right;
+                }
+            } else {
+                validate_node(cur, first, second, last);
+                cur = cur->right;
+            }
+        }
+    }
+    
+public:
+    void recoverTree(TreeNode *root) {
+        TreeNode *first = nullptr, *second = nullptr;
+        TreeNode *last = new TreeNode(INT_MIN);
+        
+        do_recover(root, first, second, last);
+        swap(first->val, second->val);
+    }
+};
+
+// Better solution: keep track of two wrong nodes
 class Solution {
     void do_recover(TreeNode *root, TreeNode* &first, TreeNode* &second, TreeNode* &last) {
         if (root == nullptr) { 
