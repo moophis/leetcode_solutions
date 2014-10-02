4b232131bf557085295e354b4e71573a7c09facd
moophis <moophis@gmail.com>
2014-10-01 20:08:07 -0700

    Word Break II: update answer, more efficient

--- Word Break II.cpp
+++ Word Break II.cpp
@@ -1,5 +1,51 @@
 // http://oj.leetcode.com/problems/word-break-ii/
 
+// new solution: use a boolean array to trim unnecessary recursive calls
+class Solution {
+    void do_break(string &s, int begin, string &buf, 
+                  vector<string> &ans, unordered_set<string> &dict, vector<bool> &possible) {
+        int size = static_cast<int>(s.size());  // size_t -> unsigned int
+        
+        if (begin >= size) {
+            int bufsize = buf.size();
+            ans.push_back(buf.substr(0, bufsize - 1));
+        }
+        
+        for (int i = 1; begin + i <= size; i++) {
+            string cur_word = s.substr(begin, i);
+            
+            if (dict.count(cur_word) > 0 && possible[begin + i]) {
+                int last_len = static_cast<int>(buf.size());
+                int ans_len = ans.size();
+                
+                buf += cur_word;
+                buf += " ";
+                do_break(s, begin + i, buf, ans, dict, possible);
+                if (ans.size() == ans_len) {  // if nothing changed, do not visit again
+                    possible[begin + i] = false;
+                }
+                buf.erase(last_len);
+            }
+        }
+    }
+
+public:
+    vector<string> wordBreak(string s, unordered_set<string> &dict) {
+        vector<string> ans;
+        string buf = "";
+        
+        if (s.size() == 0 || dict.size() == 0) {
+            return ans;
+        }
+        
+        vector<bool> possible(s.size(), true);
+        
+        do_break(s, 0, buf, ans, dict, possible);
+        return ans;
+    }
+};
+
+// old solution
 class Solution {
     void do_break(string &s, int begin, string &buf, 
                   vector<string> &ans, unordered_set<string> &dict) {
