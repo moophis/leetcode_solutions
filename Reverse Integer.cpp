24d239427c4188c06875e08a7cfc38ff72fe7c4f
moophis <moophis@gmail.com>
2014-10-03 10:03:35 -0700

    Reverse Integer: consider whether it overflows

--- Reverse Integer.cpp
+++ Reverse Integer.cpp
@@ -1,35 +1,25 @@
+// https://oj.leetcode.com/problems/reverse-integer/
+
 class Solution {
 public:
     int reverse(int x) {
-        // Note: The Solution object is instantiated only once and is reused by each test case.
-        int hi, lo, len = 1, base = 10;
-        bool sign = false;
+        bool minus = (x < 0);
+        if (minus) {
+            x = -x;
+        } 
         
-        if (x < 0) {
-            sign = true;
-            x = - x;
-        }
-        
-        while (x != 0 && x % 10 == 0)
-            x = x / 10;
+        int ret = 0;
+        while (x != 0) {
+            int now = ret * 10 + (x % 10);
+            if (now / 10 != ret) {
+                return INT_MAX;  // overflow
+            } else {
+                ret = now;
+            }
             
-        while (x / base != 0) {
-            len++;
-            base = base * 10;
+            x /= 10;
         }
         
-        for (hi = len, lo = 1; hi > lo; hi--, lo++) {
-            int base_hi = 1, base_lo = 1;
-            int h, l;
-            for (int i = 1; i < hi; i++) 
-                base_hi = base_hi * 10;
-            for (int i = 1; i < lo; i++)
-                base_lo = base_lo * 10;
-            h = (x / base_hi) % 10;
-            l = (x / base_lo) % 10;
-            x = x - h * base_hi + l * base_hi - l * base_lo + h * base_lo;
-        }
-        
-        return sign ? -x : x;
+        return minus ? -ret : ret;
     }
 };
