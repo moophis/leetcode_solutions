e30b3b8fe55255b0f7bbb89b094ce5aa756259e6
moophis <moophis@gmail.com>
2014-10-12 16:05:59 -0700

    Copy List with Random Pointer: iterative solution

--- Copy List with Random Pointer.cpp
+++ Copy List with Random Pointer.cpp
@@ -8,6 +8,45 @@
  *     RandomListNode(int x) : label(x), next(NULL), random(NULL) {}
  * };
  */
+// iterative
+class Solution {
+public:
+    RandomListNode *generate(unordered_map<RandomListNode *, RandomListNode *> &copied, RandomListNode *old) {
+        RandomListNode *node;
+        if (copied.count(old) == 0) {
+            node = new RandomListNode(old->label);
+            copied.emplace(old, node);
+        } else {
+            node = copied[old];
+        }
+        return node; 
+    } 
+    
+    RandomListNode *copyRandomList(RandomListNode *head) {
+        if (head == nullptr) {
+            return nullptr;
+        }
+        
+        unordered_map<RandomListNode *, RandomListNode *> copied;
+        RandomListNode *cur = head;
+        RandomListNode *new_head = new RandomListNode(head->label);
+        RandomListNode *new_cur = new_head;
+        while (cur != nullptr) {
+            if (cur->next != nullptr) {
+                new_cur->next = generate(copied, cur->next);
+            }
+            if (cur->random != nullptr) {
+                new_cur->random = generate(copied, cur->random);
+            }
+            cur = cur->next;
+            new_cur = new_cur->next;
+        }
+        
+        return new_head;
+    }
+};
+
+// recursion
 class Solution {
     unordered_map<int, RandomListNode *> copied;
     
