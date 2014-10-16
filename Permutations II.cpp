a402fb27b9ca0cd090d706fc77fc5da479897426
moophis <moophis@gmail.com>
2014-10-16 16:46:56 -0700

    Permutations II: do not need additional array

--- Permutations II.cpp
+++ Permutations II.cpp
@@ -1,5 +1,49 @@
 // http://oj.leetcode.com/problems/permutations-ii/
 
+// Do not need any additional data structure
+class Solution {
+    bool contains_dup(vector<int> &num, int from, int to, int value) {
+        for (int i = from; i <= to; i++) {
+            if (num[i] == value) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    void do_permute(vector<vector<int> > &ret, vector<int> &buf, vector<int> &num, int left) {
+        if (left == 0) {
+            ret.push_back(buf);
+            return;
+        }
+        
+        for (int i = 0; i < left; i++) {
+            if (!contains_dup(num, 0, i - 1, num[i])) {
+                buf.push_back(num[i]);
+                swap(num[i], num[left - 1]);
+                do_permute(ret, buf, num, left - 1);
+                swap(num[i], num[left - 1]);
+                buf.pop_back();
+            }
+        }
+    }
+    
+public:
+    vector<vector<int> > permuteUnique(vector<int> &num) {
+        vector<vector<int> > ret;
+        vector<int> buf;
+        int size = num.size();
+        if (size <= 0) {
+            return ret;
+        }
+        
+        do_permute(ret, buf, num, size);
+        
+        return ret;
+    }
+};
+
+// Use additional array to record whether a slot has been used in one function call
 class Solution {
     void do_permute(vector<vector<int> > &ret, vector<int> &num, vector<int> &buf, 
                     vector<int> &visited, int begin) {
