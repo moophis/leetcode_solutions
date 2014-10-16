69ced1723f34c2ee167e782cbba6821f5ff2e5da
moophis <moophis@gmail.com>
2014-10-15 20:57:46 -0700

    Merge Sorted Array: better solution

--- Merge Sorted Array.cpp
+++ Merge Sorted Array.cpp
@@ -5,6 +5,29 @@
  * You may assume that A has enough space to hold additional elements from B. 
  * The number of elements initialized in A and B are m and n respectively.
  */
+
+// O(n) solution
+class Solution {
+public:
+    void merge(int A[], int m, int B[], int n) {
+        if (m <= 0 && n <= 0) {
+            return;
+        }
+        
+        int cur = m + n - 1;
+        int i = m - 1, j = n - 1;
+        while (i >= 0 && j >= 0) {
+            if (A[i] > B[j]) {
+                A[cur--] = A[i--];
+            } else {
+                A[cur--] = B[j--];
+            }
+        }
+        while (j >= 0) {
+            A[cur--] = B[j--];
+        }
+    }
+};
  
 class Solution {
 public:
