7b7950670035a93160d0ab2959c1fad81347ba00
moophis <moophis@gmail.com>
2014-10-18 09:09:11 -0700

    Single Number: binary search version

--- Single Number.cpp
+++ Single Number.cpp
@@ -1,3 +1,47 @@
+// https://oj.leetcode.com/problems/single-number/
+
+// Binary search, O(nlogn) for sorting, O(logn) for searching.
+// It's especially useful when the array is sorted.
+// Idea: the odd-even property of starting index of the same numbers
+// can be different from whether the single number is ahead of them.
+class Solution {
+public:
+    int singleNumber(int A[], int n) {
+        if (n % 2 == 0) {
+            return INT_MIN;
+        }
+        
+        sort(A, A + n);
+        
+        int start = 0, end = n - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (mid == 0 || mid == n - 1) {
+                return A[mid];
+            } else if (A[mid] > A[mid-1] && A[mid] < A[mid+1]) {
+                return A[mid];
+            } else { 
+                if (mid % 2 == 0) { 
+                    if (A[mid] == A[mid+1]) {
+                        start = mid + 1;
+                    } else {
+                        end = mid - 1;
+                    }   
+                } else {
+                    if (A[mid] == A[mid+1]) {
+                        end = mid - 1;
+                    } else {
+                        start = mid + 1;
+                    }
+                }
+            }
+        }
+        
+        return INT_MIN;  // cannot find
+    }
+};
+
+// Bit manipulation: O(n), can be used in unsorted array
 class Solution {
 public:
     int singleNumber(int A[], int n) {
