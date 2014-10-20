c3b97e6ae76deae8a7affeed0412345ca4167c77
moophis <moophis@gmail.com>
2014-10-19 19:47:57 -0700

    Sort List: update code

--- Sort List.cpp
+++ Sort List.cpp
@@ -12,66 +12,57 @@
  * };
  */
 class Solution {
-public:
-    ListNode *sortList(ListNode *head) {
-        // IMPORTANT: Please reset any member data you declared, as
-        // the same Solution instance will be reused for each test case.
-        if (head == nullptr) {
-            return nullptr;
+    void mergesort(ListNode *head, ListNode *tail) {
+        if (head == nullptr || head == tail || head->next == tail) {
+            return;
+        }
+        if (head->next->next == tail) {
+            return;  // only contains one element
         }
         
-        ListNode *dummy = new ListNode(INT_MIN);
-        dummy->next = head;
-        
-        mergesort(dummy, nullptr);
-        
-        return dummy->next;
-    }
-    
-    void mergesort(ListNode *vhead, ListNode *vtail) {
-        if (vhead == nullptr || vhead == vtail || vhead->next == nullptr ||
-            vhead->next->next == nullptr || vhead->next->next == vtail) {
-            return;  // base case & error    
-        }
-        
-        ListNode *slow = vhead;
-        ListNode *fast = vhead;
-        
-        while (fast != vtail && fast->next != vtail) {
+        ListNode *slow = head, *fast = head;
+        // find middle node (slow)
+        while (fast != tail && fast->next != tail) {
             slow = slow->next;
             fast = fast->next->next;
         }
         
-        mergesort(vhead, slow->next);
-        mergesort(slow, vtail);
-        
-        ListNode *p1, *p2, *cur;
-        ListNode *overhead = new ListNode(0);
-        overhead->next = slow->next;
-        slow->next = overhead;
+        // divide
+        mergesort(head, slow->next);
+        mergesort(slow, tail);
         
         // merge
-        p1 = vhead;
-        p2 = overhead;
-        while (p1->next != overhead || p2->next != vtail) {
-            if (p1->next != overhead && p2->next != vtail) {
-                if (p1->next->val > p2->next->val) {
-                    ListNode *t = p2->next;
-                    p2->next = p2->next->next;
-                    t->next = p1->next;
-                    p1->next = t;
-                } 
-                p1 = p1->next;
-            } else if (p1->next != overhead) {
-                while (p1->next != overhead) {
-                    p1 = p1->next;
-                }
-                p1->next = vtail;
-                break;
-            } else {
-                p1->next = p2->next;
-                break;
+        ListNode bound(INT_MAX);
+        bound.next = slow->next;
+        slow->next = &bound;
+        ListNode *p1 = head;
+        ListNode *p2 = &bound;
+        while (p1->next != &bound && p2->next != tail) {
+            if (p1->next->val > p2->next->val) {
+                ListNode *t = p2->next;
+                p2->next = t->next;
+                t->next = p1->next;
+                p1->next = t;
             }
+            p1 = p1->next;
         }
+        while (p1->next != &bound) {
+            p1 = p1->next;
+        }
+        p1->next = bound.next;
+    }
+    
+public:
+    ListNode *sortList(ListNode *head) {
+        if (head == nullptr) {
+            return nullptr;
+        }
+        
+        ListNode dummy(INT_MIN);
+        dummy.next = head;
+        
+        mergesort(&dummy, nullptr);
+        
+        return dummy.next;
     }
 };
